e7b16b853522503bb7de30780a7a4a322498b1dc
M4tyKrsty <ma.cristi@duocuc.cl>
2026-08-22 00:08:15 -0400

    actualizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,20 +9,18 @@ import { MsalService } from '@azure/msal-angular';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit { // <-- Aquí está el cambio vital
+export class App implements OnInit {
   
   constructor(private msalService: MsalService) {}
 
   ngOnInit(): void {
-    // 1. Encendemos el motor de MSAL
     this.msalService.instance.initialize().then(() => {
       
-      // 2. Revisamos si venimos de regreso de Microsoft
       this.msalService.handleRedirectObservable().subscribe({
         next: (respuesta) => {
           if (respuesta) {
             console.log("¡Token recibido con éxito!", respuesta);
-            alert("¡Login Exitoso en la misma pestaña!");
+            alert("¡Login Exitoso!");
           }
         },
         error: (error) => {
