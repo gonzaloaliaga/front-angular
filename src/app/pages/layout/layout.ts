1f9bbae4fd061bfcb76549e669adef8b539e3f93
M4tyKrsty <ma.cristi@duocuc.cl>
2026-08-24 13:51:52 -0400

    prueba

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,27 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'; 
 import { AuthService } from '../../core/auth/auth';
 import { MsalService } from '@azure/msal-angular';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive], 
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
-
 export class LayoutComponent {
   
-  // Hacemos el servicio 'public' para que el HTML pueda hacerle preguntas directamente
   constructor(
-    public authService: AuthService,
+    public authService: AuthService, 
     private msalService: MsalService
   ) {}
 
   cerrarSesion() {
-    console.log("Cerrando sesión de Azure...");
     this.msalService.logoutRedirect({
       postLogoutRedirectUri: 'http://localhost:4200/login'
     });
