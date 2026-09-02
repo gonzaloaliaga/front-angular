bbafd1f726fb38c0ec279306c140c4cbd4cd6385
M4tyKrsty <ma.cristi@duocuc.cl>
2026-09-02 17:50:43 -0400

    frontend final parte 2

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -10,23 +10,43 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 })
 
 export class LayoutComponent {
+  router = inject(Router);
   isLoggedIn = signal<boolean>(false);
-  userRole = signal<string>('');
+  userRole = signal<string>(''); 
   cartCount = signal<number>(0);
   hasUnreadAlerts = signal<boolean>(true);
   consentAccepted = signal<boolean>(false);
+  perfil = signal({
+    correo: 'm4tykrsty@pedidos360.cl',
+    nombreUsuario: '',
+    alias: 'MatyCrsty',
+    ubicacion: 'La Florida, Santiago'
+  });
 
   acceptConsent() {
     this.consentAccepted.set(true);
   }
 
   loginMock() {
+    const rolDeAzure: string = 'cliente'; 
     this.isLoggedIn.set(true);
-    this.userRole.set('CLIENTE');
+    this.userRole.set(rolDeAzure);
+    const extraido = this.perfil().correo.split('@')[0];
+    this.perfil.update(p => ({ ...p, nombreUsuario: extraido }));
+    if (rolDeAzure === 'cocina') {
+      this.router.navigate(['/layout/cocina']);
+    } else if (rolDeAzure === 'auditoria') {
+      this.router.navigate(['/layout/auditoria']);
+    } else if (rolDeAzure === 'despacho') {
+      this.router.navigate(['/layout/despacho']);
+    } else {
+      this.router.navigate(['/layout/cliente']);
+    }
   }
 
   logoutMock() {
     this.isLoggedIn.set(false);
     this.userRole.set('');
+    this.router.navigate(['/layout/cliente']); 
   }
 }
