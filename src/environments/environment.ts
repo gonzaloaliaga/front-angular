5d35869312c49911e444fc80998792df0264f1e1
M4tyKrsty <ma.cristi@duocuc.cl>
2026-09-16 18:30:22 -0300

    apuntar producción al API Gateway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiInventarioUrl: 'https://pedidos360.duckdns.org/inventario/api',
-  apiPedidosUrl: 'https://pedidos360.duckdns.org/pedidos/api'
+  apiInventarioUrl: 'https://pqj1b6z9g5.execute-api.us-east-1.amazonaws.com/inventario/api',
+  apiPedidosUrl: 'https://pqj1b6z9g5.execute-api.us-east-1.amazonaws.com/pedidos/api'
 };
