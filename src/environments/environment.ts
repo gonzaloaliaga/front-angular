c2808451a963eef7cacbfb068b686dd4f1742497
Matias <ma.cristi@duocuc.cl>
2026-09-17 22:49:58 -0300

    apuntar a las rutas versionadas del API Gateway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiInventarioUrl: 'https://pqj1b6z9g5.execute-api.us-east-1.amazonaws.com/inventario/api',
-  apiPedidosUrl: 'https://pqj1b6z9g5.execute-api.us-east-1.amazonaws.com/pedidos/api'
+  apiInventarioUrl: 'https://pqj1b6z9g5.execute-api.us-east-1.amazonaws.com/dev/v1',
+  apiPedidosUrl: 'https://pqj1b6z9g5.execute-api.us-east-1.amazonaws.com/dev/v1'
 };
